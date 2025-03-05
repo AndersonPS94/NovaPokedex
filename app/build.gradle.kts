4da631569a3b63e2f10ea0ccadf660cae1215cca
AndersonPS <dson_adn@hotmail.com>
2025-03-05 12:10:54 -0300

    primeiro commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,10 +33,18 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
 
+    //room database
+
+    //retrofit
+
+    //dependencias padroes do projeto
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- app/src/main/java/com/example/hacksprintpokedex/MainActivity.kt
+++ app/src/main/java/com/example/hacksprintpokedex/MainActivity.kt
@@ -5,12 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.hacksprintpokedex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
     }
 }
