0cfc5a5797a714e586af428b4d203af04f0abf49
AndersonPS <dson_adn@hotmail.com>
2025-07-28 11:47:39 -0300

    fix: dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,66 +46,51 @@ android {
 }
 
 dependencies {
-    // Room (com KAPT)
     implementation(libs.room.runtime)
     kapt(libs.room.compiler)
     implementation(libs.room.ktx)
 
-    // Coil
     implementation(libs.coil)
     implementation(libs.coil.gif)
 
-    // Lottie
     implementation(libs.lottie)
 
-    // Paging
     implementation(libs.paging.runtime)
 
-    // Glide
     implementation(libs.glide)
     kapt(libs.glide.compiler)
 
-    // Picasso
     implementation(libs.picasso)
 
-    // Activity
     implementation(libs.androidx.activity.ktx)
 
-    // Hilt
     implementation(libs.hilt.android)
     kapt(libs.hilt.android.compiler)
 
-    // ViewModel e LiveData
     implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.lifecycle.livedata.ktx)
 
-    // SplashScreen
     implementation(libs.androidx.core.splashscreen)
 
-    // Coroutines
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
 
-    // Retrofit e OkHttp
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
     implementation(libs.logging.interceptor)
 
-    // Dependências padrões do projeto
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
 
-    // Testes Unitários
     testImplementation(libs.junit)
     testImplementation(libs.mockito.core)
     testImplementation(libs.mockito.inline)
     testImplementation(libs.androidx.core.testing)
-    testImplementation(libs.lifecycle.livedata.core.testing)
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     testImplementation(libs.room.testing)
 
-    // Testes Instrumentados
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
